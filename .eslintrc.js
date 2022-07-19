4aa68cc4a10238dd6e0b20b983de9f3b9f487320
Enricky <enricky.hipolito@gmail.com>
2022-07-19 10:24:13 -0300

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+
+  extends: [
+    // 'eslint:recommended',
+    'airbnb-base',
+    // "plugin:@typescript-eslint/eslint-recommended",
+    // "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    // tsconfigRootDir: __dirname,
+    // project: ['./tsconfig.json'],
+    // extraFileExtensions: ['.svelte']
+  },
+
+  plugins: [
+    '@typescript-eslint',
+    'svelte3',
+  ],
+
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    'indent': 'off',
+    '@typescript-eslint/indent': ['error', 2]
+  },
+
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    'svelte3/ignore-styles': () => true
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+  plugins: ['prettier-plugin-svelte'],
+  trailingComma: 'all',
+  printWidth: 120,
+}
